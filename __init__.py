e65c5e8acd0db0e9a1dc83c9a048a9d6d69381b3
JimWang151 <jim.wangs@163.com>
2025-07-08 19:31:52 +0800

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -4,20 +4,24 @@ import sys
 
 python = sys.executable
 
-from .FirstPlugin import FeishuTableReader,XMLBatchSceneReader,NewsAPI_Fetcher,Parse_News_Content
+from .FirstPlugin import FeishuTableReader,XMLBatchSceneReader,NewsAPI_Fetcher,Parse_News_Content,Parse_XML_News,String_Slicer
 
 NODE_CLASS_MAPPINGS = {
     "FeishuTableReader":FeishuTableReader,
     "XMLSceneReader":XMLBatchSceneReader,
     "NewsAPI_Fetcher":NewsAPI_Fetcher,
-    "Parse_News_Content":Parse_News_Content
+    "Parse_News_Content":Parse_News_Content,
+    "Parse_XML_News":Parse_XML_News,
+    "String_Slicer":String_Slicer
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "FeishuTableReader":"Read Scene Information",
     "XMLBatchSceneReader":"Get Scene Prompt",
     "NewsAPI_Fetcher":"Get MyShell News",
-    "Parse_News_Content":"Parse News Content"
+    "Parse_News_Content":"Parse News Content",
+    "Parse_XML_News":"Parse XML News",
+    "String_Slicer":"String Slicer"
 }
 
 
